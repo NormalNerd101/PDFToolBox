e709f9b2b110c19684e03f5c4680d957fd82ad7a
NormalNerd101 <144669891+NormalNerd101@users.noreply.github.com>
2025-01-30 23:44:59 +0700

    add class for debug for both panels

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -7,15 +7,13 @@ from PyQt6.QtGui import QFileSystemModel
 from PyQt6.QtCore import Qt
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+class OptionsPanel(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.initUI()
 
-        self.setWindowTitle("PDF Tools")
-        self.setGeometry(500, 300, 1000, 600)
-
-        # Set up central widget
-        splitter = QSplitter(Qt.Orientation.Horizontal)
+    def initUI(self):
+        self.layout = QVBoxLayout()
 
         # Set up Left panel for File Explorer
         self.file_explorer = QTreeView()
@@ -23,25 +21,49 @@ class MainWindow(QMainWindow):
 
         # connect to File System Model
         self.model = QFileSystemModel()
-        self.model.setRootPath(r"C:\Users\Windows")
+        self.model.setRootPath(r"C:/Users/Windows")
         self.file_explorer.setModel(self.model)
         root_index = self.model.index(self.model.rootPath())
         self.file_explorer.setRootIndex(root_index)
         self.file_explorer.setSelectionMode(QTreeView.SelectionMode.MultiSelection)
 
-        # Add the file explorer to the left panel
-        splitter.addWidget(self.file_explorer)
+        self.layout.addWidget(self.file_explorer)
+        self.setLayout(self.layout)
 
 
-        # Set up Right panel for feature options
-        setting_panel = QWidget()
+
+class SettingsPanel(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.initUI()
+
+    def initUI(self):
         setting_layout = QVBoxLayout()
-        setting_label = QLabel("Settings") 
+        setting_label = QLabel("Settings")
         setting_layout.addWidget(setting_label)
-        setting_panel.setLayout(setting_layout)
-        splitter.addWidget(setting_panel)
+        self.setLayout(setting_layout)
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PDF Tools")
+        self.setGeometry(500, 300, 1000, 600)
+
+        # set up the central widget
+        splitter = QSplitter(Qt.Orientation.Horizontal)
+
+        # Set up Left panel for file explorer and right panel for settings
+        self.file_explorer = OptionsPanel()
+        self.settings_panel = SettingsPanel()
+
+        splitter.addWidget(self.file_explorer)
+        splitter.addWidget(self.settings_panel)
 
         self.setCentralWidget(splitter)
+
 
 
 def main():
